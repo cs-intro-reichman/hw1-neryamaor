7bfba2983a32b22666ef096f033188358a91f9d3
נריה מאור <neri@nryhs-MacBook-Pro.local>
2025-11-04 11:49:05 +0200

    hw1-neryamaor

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		String name3 = args[2];
 		int Bill3amount = Integer.parseInt(args[3]);
 		double EachOne = Bill3amount /3.0;
-		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": Pay " + Math.ceil(EachOne) + " Shekels Each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + Math.ceil(EachOne) + " Shekels each");
 	}
 }
